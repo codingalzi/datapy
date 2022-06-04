e5fa02f64d46a539fd5845a7554516f934b95e28
Gyesik Lee <gslee@hknu.ac.kr>
2022-06-05 00:30:02 +0900

    pyplot2 almost ready

--- jupyter-book/_build/jupyter_execute/data_visualization_pyplot2.py
+++ jupyter-book/_build/jupyter_execute/data_visualization_pyplot2.py
@@ -4,6 +4,11 @@
 # (ch:pyplot2)=
 # # matplotlib.pyplot 2부
 
+# **참고**
+# 
+# 웨스 맥키니의 [<파이썬 라이브러리를 활용한 데이터 분석>](https://github.com/wesm/pydata-book)의 
+# 9장1절에 사용된 소스코드의 일부를 활용한다.
+
 # **기본 설정**
 
 # In[1]:
@@ -13,33 +18,18 @@ import numpy as np
 
 
 # 맷플롯립<font size='2'>Matplotlib</font>은 간단한 그래프 도구를 제공하는 라이브러리다. 
-
-# In[2]:
-
-
-import matplotlib
-
-
-# 설치된 버전은 다음과 같이 확인한다.
-
-# In[3]:
-
-
-matplotlib.__version__
-
-
 # 맷플롯립의 대부분의 함수는 파이플롯<font size='2'>pyplot</font> 모듈에 포함되어 있으며
 # 관행적으로 `plt` 별칭으로 불러온다.
 
-# In[4]:
+# In[2]:
 
 
 import matplotlib.pyplot as plt
 
 
-# ### `Figure` 객체와 서브플롯(subplot)
+# ## `Figure` 객체와 서브플롯(subplot)
 
-# 모든 그래프는 `Figure` 객체 내에 존재하며, `matplotlib.pyplot.figure()` 함수에 의해 생성된다.
+# 모든 그래프는 `Figure` 객체 내에 존재하며, `plt.figure()` 함수에 의해 생성된다.
 # 
 # ```python
 # fig = plt.figure()
@@ -47,85 +37,13 @@ import matplotlib.pyplot as plt
 
 # `Figure` 객체 내에 그래프를 그리려면 서브플롯(subplot)을 지정해야 한다.
 # 아래 코드는 `add_subplot()` 함수를 이용하여 지정된 `Figure` 객체안에 그래프를 그릴 공간을 준비한다.
+# (nrows, ncols, index) 형식의 인자의 의미는 다음과 같다.
 # 
-# - 인자 `(2, 2, 1)`의 의미: 2x2 크기의 공간을 확보하며, 4개의 공간 각각에 그래프를 삽입할 수 있는데
-#     그중에 첫째 공간을 가리킨다.
-#     
+# - nrows: 이미지 행의 개수
+# - ncols: 이미지 칸의 개수
+# - index: 이미지의 인덱스. 1부터 시작.
 
-# ```python
-# fig = plt.figure()
-# 
-# ax1 = fig.add_subplot(2, 1, 1)
-# ax2 = fig.add_subplot(2, 2, 2)
-# ```
-
-# In[5]:
-
-
-np.random.seed(12345)
-
-
-fig = plt.figure()
-
-ax1 = fig.add_subplot(2, 1, 1)
-ax2 = fig.add_subplot(2, 1, 2)
-
-# 위치: (2, 2, 3)
-ax2.plot(np.random.randn(50).cumsum(), 'k--')
-
-# 위치: (2, 2, 2)
-ax1.scatter(np.arange(30), np.arange(30) + 3 * np.random.randn(30))
-
-plt.show()
-
-
-# In[6]:
-
-
-np.random.seed(12345)
-
-
-fig = plt.figure()
-
-ax1 = fig.add_subplot(1, 2, 1)
-ax2 = fig.add_subplot(2, 2, 2)
-
-# 위치: (2, 2, 3)
-ax2.plot(np.random.randn(50).cumsum(), 'k--')
-
-# 위치: (2, 2, 2)
-ax1.scatter(np.arange(30), np.arange(30) + 3 * np.random.randn(30))
-
-plt.show()
-
-
-# In[7]:
-
-
-plt.rc('figure', figsize=(10, 6))  # 그림 크기 설정
-
-
-# In[8]:
-
-
-np.random.seed(12345)
-
-
-fig = plt.figure()
-
-ax1 = fig.add_subplot(1, 2, 1)
-ax2 = fig.add_subplot(2, 2, 2)
-
-# 위치: (1, 2, 1)
-ax1.scatter(np.arange(30), np.arange(30) + 3 * np.random.randn(30))
-
-# 위치: (1, 2, 2)
-ax2.plot(np.random.randn(50).cumsum(), 'k--')
-# plt.gca().set_aspect("equal")
-
-
-plt.show()
-
+# 만약 2x2 모양으로 총 4개의 이미지를 생성하려면 아래와 같이 실행한다.
 
 # ```python
 # fig = plt.figure()
@@ -136,90 +54,134 @@ plt.show()
 # ax4 = fig.add_subplot(2, 2, 4)
 # ```
 
-# #### 그래프 삽입하기
+# **그래프 삽입하기**
 
 # 두 가지 방식으로 각각의 서브플롯에 그림을 삽입할 수 있다.
 
-# ##### 방식 1: `matplotlib.pyplot.plot()` 함수 활용
+# *방식 1: `matplotlib.pyplot.plot()` 함수 활용*
 
 # 이 방식은 항상 마지막에 선언된 서브플롯에 그래프를 그린다.
 # 예를 들어 아래 코드는 무작위로 선택된 50개의 정수들의 누적합으로 이루어진 데이터를
 # 파선 그래프로 그린다.
-# 
-# __참고:__ 어레이가 주어졌을 때 해당 어레이의 누적합 어레이는 동일한 크기를 가지면서
-# 인덱스별 항목은 0번 인덱스부터 해당 인덱스의 항목까지의 합으로 지정된다.
-# 
-# - `cumsum()` 메서드: 주어진 어레이에 포함된 항목들의 누적합으로 이루어진 어레이를 반환한다.
 
-# In[9]:
-
-
-from numpy.random import randn
-
-data = np.random.randn(50).cumsum()
-
-
-# In[10]:
-
-
-data
-
-
-# In[11]:
-
-
-fig = plt.figure()
-
-ax1 = fig.add_subplot(2, 2, 1)
-ax2 = fig.add_subplot(2, 2, 2)
-ax3 = fig.add_subplot(2, 2, 3)
-
-plt.plot(data, 'k--')
-
-
-# __주의사항:__ 주피터 노트북을 사용할 경우 `matplotlib.pyplot.figure()`, 
-# `matplotlib.pyplot.add_subplot()` 등 직접 그래프를 그리고
-# 관리하는 함수들은 모두 하나의 셀에서 실행되어야 한다. 
-
-# ##### 방식 2: `객체명.plot()` 함수 활용
-
-# 특정 서브플롯에 그래프를 삽입하려면 객체 이름과 함께 `plot()` 함수 등을 호출해야 한다.
-
-# In[12]:
+# In[3]:
 
 
 np.random.seed(12345)
 
+data1 = np.random.randn(50).cumsum()
+data2 = np.random.randn(50).cumsum()
+
+
+# * 2x1 모양의 2개 이미지 사용하기
+
+# In[4]:
+
 
 fig = plt.figure()
 
+ax1 = fig.add_subplot(2, 1, 1)
+plt.plot(data1, 'k--')
+
+ax2 = fig.add_subplot(2, 1, 2)
+plt.plot(data2, 'k--')
+
+
+# * 1x2 모양의 2개 이미지 사용하기
+
+# In[5]:
+
+
+fig = plt.figure()
+
+ax1 = fig.add_subplot(1, 2, 1)
+plt.plot(data1, 'k--')
+
+ax2 = fig.add_subplot(1, 2, 2)
+plt.plot(data2, 'k--')
+
+
+# *방식 2: `객체명.plot()` 함수 활용*
+
+# 특정 서브플롯에 그래프를 삽입하려면 서브플롯 객체의 이름과 함께 `plot()` 함수 등을 호출해야 한다.
+
+# In[6]:
+
+
+np.random.seed(12345)
+
+fig = plt.figure()
 ax1 = fig.add_subplot(2, 2, 1)
 ax2 = fig.add_subplot(2, 2, 2)
 ax3 = fig.add_subplot(2, 2, 3)
-
-# 위치: (2, 2, 3)
-plt.plot(np.random.randn(50).cumsum(), 'k--')
+ax4 = fig.add_subplot(2, 2, 4)
 
 # 위치: (2, 2, 1)
-_ = ax1.hist(np.random.randn(100), bins=20, color='k', alpha=.1)
-
+ax1.hist(np.random.randn(100), bins=20, color='k', alpha=.1)
 # 위치: (2, 2, 2)
 ax2.scatter(np.arange(30), np.arange(30) + 3 * np.random.randn(30))
+# 위치: (2, 2, 3)
+ax3.plot(np.random.randn(50).cumsum(), 'k--')
+# 위치: (2, 2, 4)
+ax4.plot(np.random.randn(50).cumsum(), 'k--')
 
 plt.show()
 
 
-# #### 서브플롯 관리
+# `Figure` 객체의 크기는 `plt.rc()` 함수를 이용해서 아래와 같이 지정한다.
+# `plt.rc()` 함수의 자세한 활용법은 잠시 뒤에 보다 자세히 살펴 본다.
+
+# In[7]:
+
+
+plt.rc('figure', figsize=(10, 6))
+
+
+# 이제 네 개의 이미지가 보다 적절한 크기로 그려진다.
+
+# In[8]:
+
+
+np.random.seed(12345)
+
+fig = plt.figure()
+ax1 = fig.add_subplot(2, 2, 1)
+ax2 = fig.add_subplot(2, 2, 2)
+ax3 = fig.add_subplot(2, 2, 3)
+ax4 = fig.add_subplot(2, 2, 4)
+
+# 위치: (2, 2, 1)
+ax1.hist(np.random.randn(100), bins=20, color='k', alpha=.1)
+# 위치: (2, 2, 2)
+ax2.scatter(np.arange(30), np.arange(30) + 3 * np.random.randn(30))
+# 위치: (2, 2, 3)
+ax3.plot(np.random.randn(50).cumsum(), 'k--')
+# 위치: (2, 2, 4)
+ax4.plot(np.random.randn(50).cumsum(), 'k--')
+
+plt.show()
+
+
+# **서브플롯 관리**
 
 # `matplotlib.pyplot.subplots()` 함수는 여러 개의 서브플롯을 포함하는 `Figure` 객체를 관리해준다.
 # 예를 들어, 아래 코드는 2x3 크기의 서브플롯을 담은 (2,3) 모양의 넘파이 어레이로 생성한다.
 # 
 # * 반환값: `Figure` 객체와 지정된 크기의 넘파이 어레이. 각 항목은 서브플롯 객체임.
 
-# In[13]:
+# In[9]:
 
 
 fig, axes = plt.subplots(2, 3)
+axes
+
+
+# x 축 또는 y 축 눈금을 공유할 수 있다.
+
+# In[10]:
+
+
+fig, axes = plt.subplots(2, 3, sharex=True, sharey=True)
 axes
 
 
@@ -228,148 +190,71 @@ axes
 
 # * 여백이 0일 때:
 
-# In[14]:
+# In[11]:
 
 
 fig, axes = plt.subplots(2, 2, sharex=True, sharey=True)
 
 for i in range(2):
     for j in range(2):
-        axes[i, j].hist(np.random.randn(500), bins=50, color='k', alpha=0.5)
+        axes[i, j].plot(np.random.randn(50))
 
 plt.subplots_adjust(wspace=0, hspace=0) # 상하좌우 여백: 0
 
 
-# * 여백이 1일 때:
+# * 여백이 0.1일 때:
 
-# In[15]:
+# ## 눈금과 라벨
 
-
-fig, axes = plt.subplots(2, 2, sharex=True, sharey=True)
-
-for i in range(2):
-    for j in range(2):
-        axes[i, j].hist(np.random.randn(500), bins=50, color='k', alpha=0.5)
-
-plt.subplots_adjust(wspace=1, hspace=1)   # 상하좌우 여백: 1
-
-
-# ### 색상, 마커, 선 스타일
-
-# 그래프의 속성을 여러 방식으로 지정할 수 있다.
-
-# * 방식 1: 문자열 방식
-# 
-#     ```python
-#     ax.plot(x, y, 'g--')
-#     ```
-
-# * 방식 2: 키워드 인자 지정 방식
-# 
-#     ```python
-#     ax.plot(x, y, linestyle='--', color='g')
-#     ```
-
-# 아래 두 코드는 동일한 그림을 그려준다.
-# 단, 무작위성 때문에 무작위성의 시드(seed)를 지정해줘야 한다.
-
-# In[16]:
-
-
-np.random.seed(12345)
-
-plt.plot(randn(30).cumsum(), 'ko--')
-
-
-# In[17]:
-
-
-np.random.seed(12345)
-plt.plot(randn(30).cumsum(), color='k', linestyle='dashed', marker='o')
-
-
-# In[18]:
-
-
-np.random.seed(12345)
-plt.plot(randn(30).cumsum(), color='#d25c6a', linestyle='--', marker='o')
-
-
-# 색상은 RGB 값을 HEX 코드를 이용하여 직접 사용할 수도 있다.
-# 
-# __참고:__ [RGB 색상의 HEX 코드 찾기](https://www.w3schools.com/colors/colors_rgb.asp)
-
-# #### 여러 그래프 하나의 서브플롯에 그리기
-
-# 여러 스타일의 그래프를 하나의 서브플롯에 그려 다양한 정보를 동시에 전달할 수도 있다.
-
-# In[19]:
-
-
-np.random.seed(12345)
-data = np.random.randn(30).cumsum()
-
-
-# In[20]:
-
-
-plt.plot(data, 'k--', label='Default')
-
-
-# In[21]:
-
-
-plt.plot(data, 'k-', drawstyle='steps-post', label='steps-post')
-
-
-# `plt.legend()` 함수는 각 그래프의 `label` 속성 정보를 이용하여 범례를 적절한 곳에 생성한다.
-# 
-# - `loc='best'`: 범례 위지 지정. 기본값은 가장 적절한 곳으로 자동 지정.
-
-# In[22]:
-
-
-plt.plot(data, 'k--', label='Default')
-plt.plot(data, 'k-', drawstyle='steps-post', label='steps-post')
-
-plt.legend(loc='best')  # 범례 추가
-
-
-# ### 눈금과 라벨
-
-# #### 이미지 타이틀, 축 이름, 눈금, 눈금 이름 지정
+# **이미지 타이틀, 축 이름, 눈금, 눈금 이름 지정**
 
 # 두 가지 방식으로 진행할 수 있다.
 
-# ##### 방식 1: 파이플롯 객체의 메서드 활용
+# *방식 1: 파이플롯 객체의 메서드 활용*
 
 # - `set_xticks()` 함수: 눈금 지정
 # - `set_xticklabels()` 함수: 눈금 라벨 지정
 # - `set_title()` 함수: 그래프 타이틀 지정
 # - `set_xlabel()` 함수: x축 이름 지정
 
-# In[23]:
+# In[12]:
 
 
-data = np.random.randn(1000).cumsum()
+data = np.random.randn(100).cumsum()
 
 
-# In[24]:
+# In[13]:
 
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 ax.plot(data)
 
-ticks = ax.set_xticks([0, 250, 500, 750, 1000])
+ticks = ax.set_xticks([0, 25, 50, 75, 100])
+labels = ax.set_xticklabels(['one', 'two', 'three', 'four', 'five'])
+
+ax.set_title('Random Plot')
+ax.set_xlabel('Levels')
+
+
+# 눈금 크기와 방향도 지정할 수 있다.
+
+# In[14]:
+
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.plot(data)
+
+ticks = ax.set_xticks([0, 25, 50, 75, 100])
 labels = ax.set_xticklabels(['one', 'two', 'three', 'four', 'five'],
-                            rotation=30, fontsize='small')
+                            rotation=90, fontsize='small')
 
-ax.set_title('My first matplotlib plot')
-ax.set_xlabel('Stages')
+ax.set_title('Random Plot')
+ax.set_xlabel('Levels')
 
 
-# ##### 방식 2: pyplot 모듈의 함수 활용
+# *방식 2: pyplot 모듈의 함수 활용*
 
 # 이 방식은 마지막에 선언된 서브플롯에 대해서만 작동한다.
 # 
@@ -377,35 +262,37 @@ ax.set_xlabel('Stages')
 # - `plt.title()` 함수: 그래프 타이틀 지정
 # - `plt.xlabel()` 함수: x축 이름 지정
 
-# In[25]:
+# In[15]:
 
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 ax.plot(data)
 
-plt.xticks([0, 250, 500, 750, 1000], ['one', 'two', 'three', 'four', 'five'],
-            rotation=30, fontsize='small')
+plt.xticks([0, 25, 50, 75, 100], ['one', 'two', 'three', 'four', 'five'],
+            rotation=90, fontsize='small')
 
-plt.title('My first matplotlib plot')
-plt.xlabel('Stages')
+ax.set_title('Random Plot')
+ax.set_xlabel('Levels')
 
 
-# ### 서브플롯에 주석 및 도형 추가하기
+# ## 주석
 
-# #### 주석 추가
-
+# 이미지에 주석을 추가할 수 있다.
+# 
 # 설명을 위해 S&P 500 (스탠다드 앤 푸어스, Standard and Poor's 500)의 미국 500대 기업을 포함한 
 # 주식시장지수 데이터로 그래프를 생성하고 2007-2008년 사이에 있었던 
 # 재정위기와 관련된 중요한 날짜를 주석으로 추가한다.
 
-# In[26]:
+# In[16]:
 
 
-spx_path = 'https://raw.githubusercontent.com/codingalzi/pydata/master/notebooks/examples/spx.csv'
+import pandas as pd
+
+spx_path = 'https://raw.githubusercontent.com/codingalzi/datapy/master/jupyter-book/examples/spx.csv'
 
 
-# In[27]:
+# In[17]:
 
 
 data = pd.read_csv(spx_path)
@@ -417,7 +304,7 @@ data
 # - `index_col=0`: 0번 열(column)을 인덱스로 사용
 # - `parse_dates=True`: 년월일까지만 구분해서 인덱스로 사용하도록 함. 기본값은 `False`.
 
-# In[25]:
+# In[18]:
 
 
 data = pd.read_csv(spx_path, index_col=0, parse_dates=True)
@@ -434,7 +321,7 @@ data
 # 
 # __참고:__ 반드시 필요한 과정은 아니다. `spx` 대신 `data`를 그대로 사용해도 동일하게 작동한다.
 
-# In[26]:
+# In[19]:
 
 
 spx = data['SPX']
@@ -442,10 +329,21 @@ spx
 
 
 # 위 데이터를 단순하게 그래프로 나타내면 다음과 같다.
-# 
-# __참고:__ 판다스의 시리즈와 데이터프레임은 자체적으로 `plot()` 메서드를 지원한다.
 
-# In[27]:
+# In[20]:
+
+
+from datetime import datetime
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+
+ax.plot(spx, 'k-')
+
+
+# **참고:** 판다스와 시리즈 자체로 그래프 그리기 기능을 지원한다. 
+
+# In[21]:
 
 
 from datetime import datetime
@@ -463,7 +361,7 @@ spx.plot(ax=ax, style='k-')
 # - 2008년 3월 12일: 베어스턴스 투자은행 붕괴
 # - 2008년 9월 15일: 레만 투자은행 파산
 
-# In[28]:
+# In[22]:
 
 
 crisis_data = [
@@ -473,7 +371,7 @@ crisis_data = [
 ]
 
 
-# #### `annotate()` 메서드 활용
+# **`annotate()` 메서드 활용**
 
 # - `xt` 속성: 화살표 머리 위치
 # - `xytext` 속성: 텍스트 위치
@@ -481,7 +379,7 @@ crisis_data = [
 # - `horizontalalignment`: 텍스트 좌우 줄맞춤
 # - `verticalalignment`: 텍스트 상하 줄맞춤
 
-# In[29]:
+# In[23]:
 
 
 from datetime import datetime
@@ -506,101 +404,7 @@ ax.set_ylim([600, 1800])
 ax.set_title('Important dates in the 2008-2009 financial crisis')
 
 
-# #### 도형 추가
-
-# 지정된 함수를 이용하여 다양한 도형을 추가할 수 있다.
-# 
-# - `plt.Rectangle()`: 직사각형. 왼편 아래 좌표 및 가로 세로 길이
-# - `plt.Circle()`: 원. 원의 중심 좌표 및 반지름.
-# - `plt.Polygon()`: 다각형. 각 꼭지점의 좌표.
-
-# __주의사항:__ x, y축의 눈금이 다르면 도형이 정확하게 표현되지 않을 수 있다.
-# 예를 들어, 아래에서 원이 타원으로 보이는 이유가 두 축의 척도가 다르기 때문이다.
-
-# In[30]:
-
-
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-
-rect = plt.Rectangle((0.2, 0.75), 0.4, 0.15, color='k', alpha=0.3)
-circ = plt.Circle((0.7, 0.2), 0.15, color='b', alpha=0.3)
-pgon = plt.Polygon([[0.15, 0.15], [0.35, 0.4], [0.2, 0.6]],
-                   color='g', alpha=0.5)
-
-ax.add_patch(rect)
-ax.add_patch(circ)
-ax.add_patch(pgon)
-
-
-# 척도를 맞추는 방법으로 `Figure` 객체를 생성할 때 두 축의 크기를 동일하게 지정하면 된다.
-# 
-# - `figsize` 키워드 인자 활용
-
-# In[31]:
-
-
-fig = plt.figure(figsize=(10, 10))
-
-ax = fig.add_subplot(1, 1, 1)
-rect = plt.Rectangle((0.2, 0.75), 0.4, 0.15, color='k', alpha=0.3)
-circ = plt.Circle((0.7, 0.2), 0.15, color='b', alpha=0.3)
-pgon = plt.Polygon([[0.15, 0.15], [0.35, 0.4], [0.2, 0.6]],
-                   color='g', alpha=0.5)
-ax.add_patch(rect)
-ax.add_patch(circ)
-ax.add_patch(pgon)
-
-
-# In[32]:
-
-
-np.random.seed(12345)
-
-
-fig = plt.figure(figsize=(10, 5))
-
-ax1 = fig.add_subplot(1, 2, 1)
-ax2 = fig.add_subplot(1, 2, 2)
-
-# 위치: (1, 2, 1)
-ax1.hist(np.random.randn(100), bins=20, color='k', alpha=.3)
-
-# 위치: (1, 2, 2)
-ax2.scatter(np.arange(30), np.arange(30) + 3 * np.random.randn(30))
-
-# 도형 추가
-circ = plt.Circle((10, 10), 5, color='b', alpha=0.3)
-ax2.add_patch(circ)
-    
-
-plt.show()
-
-
-# ### 그래프 저장하기
-
-# `plt.savefig()` 함수를 이용하여 그래프를 원하는 포맷의 파일로 저장할 수 있다.
-# 
-# - `dpi=100`: 인치당 픽셀 수. 저장된 이미지의 해상도 지정.
-# - `bbox_inches=tight`: 이미지별 둘레의 공백 지정. 여기서는 공백을 거의 사용하지 말라는 의미임.
-
-# In[33]:
-
-
-fig = plt.figure(figsize=(5, 5))
-ax = fig.add_subplot(1, 1, 1)
-rect = plt.Rectangle((0.2, 0.75), 0.4, 0.15, color='k', alpha=0.3)
-circ = plt.Circle((0.7, 0.2), 0.15, color='b', alpha=0.3)
-pgon = plt.Polygon([[0.15, 0.15], [0.35, 0.4], [0.2, 0.6]],
-                   color='g', alpha=0.5)
-ax.add_patch(rect)
-ax.add_patch(circ)
-ax.add_patch(pgon)
-
-plt.savefig('figpath.png', dpi=100, bbox_inches='tight')
-
-
-# ### matplotlib 기본 설정
+# ## matplotlib 기본 설정
 
 # `plt.rc()` 함수를 이용하여 matplot을 이용하여 생성되는 이미지 관련 설정을 전역적으로 지정할 수 있다.
 # 사용되는 형식은 다음과 같다.
@@ -615,31 +419,42 @@ plt.savefig('figpath.png', dpi=100, bbox_inches='tight')
 # 
 # 아래 코드는 이미지의 사이즈를 지정한다.
 
-# In[34]:
+# In[24]:
 
 
-plt.rc('figure', figsize=(10, 10))
+plt.rc('figure', figsize=(6, 6))
 
 
-# In[35]:
+# In[25]:
 
+
+from datetime import datetime
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
-rect = plt.Rectangle((0.2, 0.75), 0.4, 0.15, color='k', alpha=0.3)
-circ = plt.Circle((0.7, 0.2), 0.15, color='b', alpha=0.3)
-pgon = plt.Polygon([[0.15, 0.15], [0.35, 0.4], [0.2, 0.6]],
-                   color='g', alpha=0.5)
-ax.add_patch(rect)
-ax.add_patch(circ)
-ax.add_patch(pgon)
+
+spx.plot(ax=ax, style='k-')
+
+for date, label in crisis_data:
+    ax.annotate(label, 
+                xy=(date, spx.asof(date) + 75),
+                xytext=(date, spx.asof(date) + 225),
+                arrowprops=dict(facecolor='black', headwidth=4, width=2,
+                                headlength=4),
+                horizontalalignment='left', verticalalignment='top')
+
+# 2007-2010 사이로 확대
+ax.set_xlim(['1/1/2007', '1/1/2011'])
+ax.set_ylim([600, 1800])
+
+ax.set_title('Important dates in the 2008-2009 financial crisis')
 
 
 # 아래 코드는 다양한 속성을 지정하는 방식을 보여준다.
 
 # * 이미지 사이즈 지정
 
-# In[36]:
+# In[26]:
 
 
 plt.rc('figure', figsize=(10, 6))
@@ -647,7 +462,7 @@ plt.rc('figure', figsize=(10, 6))
 
 # * 선 속성 지정
 
-# In[37]:
+# In[27]:
 
 
 plt.rc('lines', linewidth=3, color='b')
@@ -655,7 +470,7 @@ plt.rc('lines', linewidth=3, color='b')
 
 # * 텍스트 폰트 속성 지정
 
-# In[38]:
+# In[28]:
 
 
 font_options = {'family' : 'monospace',
@@ -666,7 +481,7 @@ plt.rc('font', **font_options)
 
 # * 그래프 구성 요소의 색상 지정
 
-# In[39]:
+# In[29]:
 
 
 plt.rcParams['text.color'] = 'blue'
@@ -677,7 +492,7 @@ plt.rcParams['ytick.color'] = '#CD5C5C'  # RGB 색상
 
 # 아래 코드는 앞서 설정된 다양한 속성을 반영한 결과를 보여준다.
 
-# In[40]:
+# In[30]:
 
 
 from datetime import datetime
@@ -703,8 +518,6 @@ ax.set_ylim([600, 1800])
 ax.set_title('Important dates in the 2008-2009 financial crisis')
 
 
-# In[ ]:
+# ## 연습문제
 
-
-
-
+# 참고: [(실습) matplotlib.pyplot 2부](https://colab.research.google.com/github/codingalzi/datapy/blob/master/practices/practice-visualization_pyplot2.ipynb)
